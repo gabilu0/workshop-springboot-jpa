fa15e4884f161c7208dd87626a1e9c9cffd4d9bc
Gabriel Santos <gabilubastos@gmail.com>
2023-07-29 15:10:06 -0300

    Implementação da damada de serviço

--- src/main/java/com/artenal/course/repositories/UserRepository.java
+++ src/main/java/com/artenal/course/repositories/UserRepository.java
@@ -3,7 +3,7 @@ package com.artenal.course.repositories;
 import com.artenal.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>{
 
 
 }
