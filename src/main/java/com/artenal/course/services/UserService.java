52ea4c06ffd0ad3817f7e4ff114a4d325dcbd351
Gabriel Santos <gabilubastos@gmail.com>
2023-08-03 16:25:34 -0300

    Tratamento de exceção findByid

--- src/main/java/com/artenal/course/services/UserService.java
+++ src/main/java/com/artenal/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.artenal.course.services;
 
 import com.artenal.course.entities.User;
 import com.artenal.course.repositories.UserRepository;
+import com.artenal.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
@@ -22,7 +23,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
